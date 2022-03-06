f41fd8d6f0c40227d0e1b2a1a87d816e6c391354
Shihab <73121069+maker-shihab@users.noreply.github.com>
2022-03-07 01:18:38 +0600

    Js Added
    Weather Condition check

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const API_KEY = `bd9af946dfba173d71c145ba85b00064`;
+const searchTemperature = () => {
+  const city = document.getElementById('city-name').value;
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+  console.log(url);
+  
+  fetch(url)
+  .then(res => res.json())
+  .then(data => displayTemperature(data))
+}  
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+const displayTemperature = temperature => {
+    setInnerText('city', temperature.name);
+    setInnerText('condition', temperature.main.temp);
+    setInnerText('wether-helth', temperature.weather[0].main);
+    console.log(temperature);
+    // Set weather Icon
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', url);
+}
